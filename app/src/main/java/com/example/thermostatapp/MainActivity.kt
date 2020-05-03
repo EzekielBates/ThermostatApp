c4f52fef814695da5e0f827d6219f9b0bd8394cf
Ezekiel Bates <bateszeke@gmail.com>
2020-05-03 10:40:25 -0500

    Push temperature data to database.

--- app/src/main/java/com/example/thermostatapp/MainActivity.kt
+++ app/src/main/java/com/example/thermostatapp/MainActivity.kt
@@ -3,14 +3,12 @@ package com.example.thermostatapp
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import android.widget.ToggleButton
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModel
 import com.google.gson.Gson
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 import org.springframework.http.converter.StringHttpMessageConverter
 import org.springframework.web.client.RestTemplate
 
@@ -33,10 +31,11 @@ class MainActivity : AppCompatActivity() {
 
         setTemp = findViewById(R.id.setTemp)
         currentTemp = findViewById(R.id.currentTemp)
-        power = findViewById(R.id.toggleButton4)
-        cool = findViewById(R.id.toggleButton6)
+        power = findViewById(R.id.toggleButton6)
+        cool = findViewById(R.id.toggleButton4)
 
-        var t = GetData()
+
+        val t = GetData()
         t.launchDataLoad("http://192.168.1.66:8000/thermostat/thermostatsettings/get/1",temperatureInfo,setTemp,currentTemp,power,cool)
 
         increase = findViewById(R.id.increase)
@@ -52,12 +51,80 @@ class MainActivity : AppCompatActivity() {
             if(temperatureInfo.temperature > 50) {
                 temperatureInfo.temperature--
                 updateSetTemp(setTemp, temperatureInfo)
+
             }
         }
+
+        power.setOnClickListener{
+            if(!power.isChecked){
+                cool.isEnabled = false
+                updateSetPower(false,temperatureInfo)
+            }
+            else if(power.isChecked){
+                cool.isEnabled = true
+                updateSetPower(true,temperatureInfo)
+            }
+        }
+
+        cool.setOnClickListener{
+            if(!cool.isChecked ){
+                updateSetCool(false,temperatureInfo)
+            }
+            else if(cool.isChecked){
+                updateSetCool(true,temperatureInfo)
+            }
+        }
+
+
+
+    }
+
+    private fun updateSetCool(horc : Boolean,temperatureInfo: TemperatureData){
+        val s = SetData()
+        temperatureInfo.hotorcold = horc
+        s.launchDataLoad(temperatureInfo)
+    }
+
+    private fun updateSetPower(onoroff : Boolean,temperatureInfo: TemperatureData){
+        val s = SetData()
+        temperatureInfo.onoroff = onoroff
+        s.launchDataLoad(temperatureInfo)
     }
 
     private fun updateSetTemp(textView: TextView, temperatureInfo: TemperatureData){
+        val s = SetData()
         textView.text = temperatureInfo.temperature.toString()
+        s.launchDataLoad(temperatureInfo)
+
+    }
+
+    class SetData : ViewModel (){
+
+        private val completableJob = Job()
+        private val coroutineScope = CoroutineScope(Dispatchers.IO + completableJob)
+
+        fun launchDataLoad(tempInfo:TemperatureData){
+            coroutineScope.launch{
+                val restTemplate = RestTemplate()
+
+                restTemplate.messageConverters.add(StringHttpMessageConverter())
+                var oof = "True"
+                var horc = "True"
+
+                if(tempInfo.onoroff == false){
+                    oof = "False"
+                }
+
+                if(tempInfo.hotorcold == false){
+                    horc = "False"
+                }
+
+
+                val url = "http://192.168.1.66:8000/thermostat/thermostatsettings/set/1?temp=" + tempInfo.temperature.toString() + "&horc=" + horc + "&oof=" + oof + "&htemp=" + tempInfo.housetemp.toString()
+                restTemplate.getForObject(url, String::class.java, "Android")
+
+            }
+        }
     }
 
     class GetData : ViewModel() {
@@ -78,13 +145,15 @@ class MainActivity : AppCompatActivity() {
             completableJob.cancel()
         }
 
-        fun update(setTemp:TextView,currentTemp:TextView,temperatureInfo: TemperatureData,power: ToggleButton,cool: ToggleButton){
+        private fun update(setTemp:TextView, currentTemp:TextView, temperatureInfo: TemperatureData, power: ToggleButton, cool: ToggleButton){
             setTemp.text = temperatureInfo.temperature.toString()
             currentTemp.text = temperatureInfo.housetemp.toString()
+            //power.isPressed = temperatureInfo.onoroff
+            //cool.isPressed = temperatureInfo.hotorcold
 
         }
 
-        fun request(url: String, temperatureInfo: TemperatureData?){
+        private fun request(url: String, temperatureInfo: TemperatureData?){
             // Create a new RestTemplate instance
             // Create a new RestTemplate instance
             val restTemplate = RestTemplate()
@@ -97,7 +166,7 @@ class MainActivity : AppCompatActivity() {
             // Make the HTTP GET request, marshaling the response to a String
             val result =
                 restTemplate.getForObject(url, String::class.java, "Android")
-            var json = Gson().fromJson(result,TemperatureData::class.java)
+            val json = Gson().fromJson(result,TemperatureData::class.java)
             temperatureInfo?.temperature = json.temperature
             temperatureInfo?.hotorcold = json.hotorcold
             temperatureInfo?.housetemp = json.housetemp
